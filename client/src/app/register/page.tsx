67c4a9f858ac40f1c2d389edf316ec0a7ede48aa
SorrawitS <83335817+sorrawit2546@users.noreply.github.com>
2025-01-18 22:56:55 +0700

    <desigh><register Page>

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,7 +1,10 @@
 'use client'
+import Header from "@/components/header";
 import "@/styles/globals.css";
 import axios from "axios";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { useEffect, useState } from "react";
+import { Terminal } from "lucide-react";
 
 interface User {
   email: string,
@@ -22,12 +25,17 @@ export default function RegisterPage() {
   const handleRegister = async (e: any) => {
     try {
       e.preventDefault();
-      const data = await axios.post('http://localhost:4000/api/register', {
-        email,
-        name,
-        password
-      });
-      
+      if (!email || !name || !password) {
+        console.log("pls. check Data!")
+      }
+      if (email && name && password) {
+        const data = await axios.post('http://localhost:4000/api/register', {
+          email,
+          name,
+          password
+        });
+      }
+
     } catch (error) {
 
     }
@@ -35,34 +43,47 @@ export default function RegisterPage() {
 
 
   return (
-    <div className="flex justify-self-center mt-60">
-      <form onSubmit={handleRegister}>
-        <div className="flex flex-row w-[980px] h-full shadow-2xl  rounded-xl space-x-20">
-          <div className="flex w-[490px] h-96 shadow-2xl rounded-xl justify-center items-center">
-            <img className="w-[300px] h-[300px]" src="/logo9sorrawit.png" alt="" />
+    <div className="font-manrope font-bold space-y-3">
+      <Header />
+      <Alert className="flex flex-col justify-self-center w-[800px]">
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+      <div className="flex justify-self-center">
+        <form onSubmit={handleRegister}>
+          <div className="flex flex-row w-[980px] h-[500px] shadow-2xl  rounded-xl space-x-20">
+            <div className="flex w-[490px] h-full shadow-2xl rounded-xl justify-center items-center">
+              <img className="w-[300px] h-[300px]" src="/logo9sorrawit.png" alt="" />
+            </div>
+            <div className="flex flex-col justify-center items-start space-y-2">
+              <div className="">
+                Email
+              </div>
+              <div className="flex flex-row-10">
+                <input className="h-10 pl-2  border-black rounded-lg shadow-lg" type="email" name="email" value={email} onChange={(e: any) => setEmail(e.target.value)} placeholder="example@gmail.com" />
+              </div>
+              <div>
+                Name
+              </div>
+              <div className="flex flex-row ">
+                <input className="h-10 pl-2  border-black rounded-lg shadow-lg" type="name" name="name" value={name} onChange={(e: any) => setName(e.target.value)} placeholder="johnDoe" />
+              </div>
+              <div>
+                Password
+              </div>
+              <div className="flex flex-row ">
+                <input className="h-10 pl-2  border-black rounded-lg shadow-lg" type="password" name="password" value={password} onChange={(e: any) => setPassword(e.target.value)} placeholder="Input password" />
+              </div>
+              <div className="flex justify-center items-center">
+                <button className="bg-white dark:bg-black dark:hover:hover:bg-sky-400  dark:text-white shadow-2xl rounded-3xl w-40 h-10 mt-5" type="submit">Register</button>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-col justify-center items-center space-y-5">
-            <div className="flex flex-row-10">
-              Email
-              <input className="ml-2 pl-2 border-[2px] border-black rounded-lg shadow-lg" type="email" name="email" value={email} onChange={(e: any) => setEmail(e.target.value)} placeholder="Pls.Input Email" />
-            </div>
-
-            <div className="flex flex-row ">
-              Name
-              <input className="ml-2 pl-2 border-[2px] border-black rounded-lg shadow-lg" type="name" name="name" value={name} onChange={(e: any) => setName(e.target.value)} placeholder="Pls.Input Name" />
-            </div>
-
-            <div className="flex flex-row ">
-              Password
-              <input className="ml-2 pl-2 border-[2px] border-black rounded-lg shadow-lg" type="password" name="password" value={password} onChange={(e: any) => setPassword(e.target.value)} placeholder="Pls.Input password" />
-            </div>
-
-            <div className="flex justify-center items-center">
-              <button className="bg-white shadow-2xl rounded-3xl w-40 h-10 mt-10" type="submit">Register</button>
-            </div>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   )
 }
