df3c08e6fb1ece2b53dd5f13ead8f5c40686211a
SorrawitS <sangmanee773@gmail.com>
2025-04-30 17:46:21 +0700

    client:change font-color

--- client/src/components/banner.tsx
+++ client/src/components/banner.tsx
@@ -12,12 +12,12 @@ export default function Banner() {
     }, []);
 
     return (
-        <div data-aos="fade-up" data-aos-duration="3000" className="font-manrope w-full  bg-gradient-to-r from-[#F6EBE6] to-[#AEE1F9] justify-self-center ">
+        <div data-aos="fade-up" data-aos-duration="3000" className="font-manrope w-full  bg-gradient-to-r from-[#F6EBE6] to-[#AEE1F9] justify-self-center dark:bg-gradient-to-r dark:from-[#000000] dark:to-[#09658f]">
             <Header />
             <div className="max-w-full w-auto sm:pl-40 sm:pr-40 lg:w-6/6 2xl:w-6/6 pl-64 pr-64 pt-16 justify-self-center justify-center">
                 <label htmlFor="" className="max-w-full flex text-[48px]  font-semibold ">💬Hey, I’m 9Sorrawit Sangmanee</label>
-                <label htmlFor="" className="max-w-full flex mb:flex-col sm:flex-col md:flex-row  text-[64px] font-bold  bg-gradient-to-r from-[#000000] to-[#0D00FF] inline-block text-transparent bg-clip-text">UX/UI Design / Front-end Developer</label>
-                <label htmlFor="" className="max-w-full flex mb:flex-col sm:flex-col md:flex-row md:text-[35px] lg:text-[40px] text-[40px]  font-bold  ">Graduate from <span className="bg-gradient-to-r from-[#C800FF] to-[#016466] inline-block text-transparent bg-clip-text">&nbsp;Software Engineering #12&nbsp;</span> BUU</label>
+                <label htmlFor="" className="max-w-full flex mb:flex-col sm:flex-col md:flex-row  text-[64px] font-bold  bg-gradient-to-r from-[#000000] to-[#0D00FF] dark:bg-gradient-to-r dark:from-[#ffffff] dark:to-[#c8ff00] inline-block text-transparent bg-clip-text">UX/UI Design / Front-end Developer</label>
+                <label htmlFor="" className="max-w-full flex mb:flex-col sm:flex-col md:flex-row md:text-[35px] lg:text-[40px] text-[40px]  font-bold  ">Graduate from <span className="bg-gradient-to-r from-[#C800FF] to-[#016466] dark:bg-gradient-to-r dark:from-[#ffffff] dark:to-[#c8ff00] inline-block text-transparent bg-clip-text">&nbsp;Software Engineering #12&nbsp;</span> BUU</label>
                 <div className="flex max-w-full justify-self-end lg:w-[500px] lg:h-[500px] md:w-80 md:h-80 sm:w-60 sm:h-60 mt-3   bg-[#FFFFFF]  rounded-full shadow-2xl">
                     <Image className='rounded-full' src="/cat.webp" alt="" width={500} height={500} />
                 </div>
