b18325a1442bdab42184a838514ad95f80d24edd
SorrawitS <sangmanee773@gmail.com>
2025-05-03 23:47:32 +0700

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,27 @@
+// ./client/next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     serverActions: true,
   },
+
+  // --- เพิ่มส่วนนี้เข้าไป ---
+  eslint: {
+    // คำเตือน: การตั้งค่านี้จะทำให้ Production Build ผ่านได้
+    // แม้ว่าโปรเจกต์ของคุณจะมี ESLint errors ก็ตาม
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! คำเตือน !!
+    // อนุญาตให้ Production Build ผ่านได้อย่างอันตราย
+    // แม้ว่าโปรเจกต์ของคุณจะมี Type errors ก็ตาม
+    // !! คำเตือน !!
+    ignoreBuildErrors: true,
+  },
+  // -----------------------
+
 };
 
 module.exports = nextConfig;
